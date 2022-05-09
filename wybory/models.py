0d46b1eec7eb7a21b1b6d96eb3d6b80882fdec32
Piotr Szuflicki <pszuflicki@lbpro.pl>
2022-05-09 10:38:54 +0200

    Initial commit

---
+++ wybory/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Wybory(models.Model):
+    nazwa = models.CharField(max_length=50)
+    poczatekWyborow = models.DateTimeField()
+    koniecWyborow = models.DateTimeField()
+    maxKandydatow = models.IntegerField()
+
+class Osoba(models.Model):
+    imie = models.CharField(max_length=20)
+    nazwisko = models.CharField(max_length=50)
+
+class OsobaWybory(models.Model):
+    wyboryId = models.ForeignKey(Wybory, on_delete=models.CASCADE)
+    OsobaId = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+    czyOddalGlos = models.BooleanField(default=False)
+    czyKandydat = models.BooleanField(default=False)
+
+class Glos(models.Model):
+    wyboryId = models.ForeignKey(Wybory)
+    kandydatOsobaID = models.ForeignKey(Osoba)
