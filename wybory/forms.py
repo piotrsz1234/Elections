dd6c4527b4bc804a48b788e96ee510816665db8c
Piotr Szuflicki <pszuflicki@lbpro.pl>
2022-06-12 23:49:41 +0200

    Change views and add some styling

--- wybory/forms.py
+++ wybory/forms.py
@@ -9,7 +9,6 @@ class VoteForm(forms.Form):
     def __init__(self, candidates, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['kandydaci'] = forms.ChoiceField(choices=candidates, widget=forms.RadioSelect)
-
     kandydaci = forms.ChoiceField()
 
 
